5bbe6f672015c3828797c14dabbc8d8136cf68d5
xbl4z3r <67677932+xbl4z3r@users.noreply.github.com>
2025-03-24 18:31:39 +0200

    Readme, License, Footer & Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {ThemeProvider} from "next-themes";
+import {Analytics} from "@vercel/analytics/next";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -28,6 +30,8 @@ export default function RootLayout({
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider attribute="class">
             {children}
+            <Footer/>
+            <Analytics/>
         </ThemeProvider>
         </body>
         </html>
